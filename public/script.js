3638d6eebf04aa617f48c60803116b9893f648f5
deviant <mb.deviant@gmail.com>
2024-06-09 01:20:13 +0300

    create initial fetch function to display data

--- public/script.js
+++ public/script.js
@@ -1,8 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("/data")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => console.error("Error fetching data:", error));
+const data = [];
+
+document.addEventListener("DOMContentLoaded", async () => {
+  console.log("dom content parsed and loaded successfully");
+  try {
+    const res = await fetch("/data");
+    const fetchedData = await res.json();
+
+    fetchedData.forEach((item) => {
+      data.push(item);
+    });
+  } catch {
+    console.error("error fetching data", error);
+  }
 });
+
+function log() {
+  console.log("Initial data:", data);
+  console.log(data[0].description[1]);
+
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+  }
+}
