0ddefdba8108b3fa1d2936aefdab0df34449e0c7
deviant <mb.deviant@gmail.com>
2024-06-09 03:09:11 +0300

    remove debugging log

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 const data = [];
 
 document.addEventListener("DOMContentLoaded", async () => {
-  console.log("dom content parsed and loaded successfully");
   try {
     const res = await fetch("/data");
     const fetchedData = await res.json();
