2511aa0e41993de504c061fae190d62b5a1dd661
deviant <mb.deviant@gmail.com>
2024-06-09 03:08:36 +0300

    create initial data appending function

--- public/script.js
+++ public/script.js
@@ -12,5 +12,28 @@ document.addEventListener("DOMContentLoaded", async () => {
   } catch {
     console.error("error fetching data", error);
   }
+  appendData();
 });
 
+function appendData() {
+  const root = document.getElementById("root");
+
+  for (const key in data) {
+    const tourItem = document.createElement("div");
+
+    const itemTitle = document.createElement("h2");
+    itemTitle.textContent = data[key].title;
+
+    const descriptions = document.createElement("ul");
+
+    for (const description of data[key].description) {
+      const descItem = document.createElement("li");
+      descItem.textContent = description;
+      descriptions.appendChild(descItem);
+    }
+
+    tourItem.appendChild(itemTitle);
+    tourItem.appendChild(descriptions);
+    root.appendChild(tourItem);
+  }
+}
