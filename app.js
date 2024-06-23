23e6f241c69d36999253ae869f4476d18da16295
deviant <mb.deviant@gmail.com>
2024-06-23 22:02:06 +0300

    edit get method for data to send json file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require("cors");
+const fs = require("fs");
 require("dotenv").config();
 
 const port = process.env.port || 3000;
@@ -15,8 +16,26 @@ app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
+// app.get("/data", (req, res) => {
+//   res.sendFile(path.join(__dirname, "data.json"));
+// });
+
 app.get("/data", (req, res) => {
-  res.sendFile(path.join(__dirname, "data.json"));
+  const filePath = path.join(__dirname, "data.json");
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("error reading data file");
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    } catch (error) {
+      console.error("error parsing data", error);
+      res.status(500).send("error parsing data");
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
